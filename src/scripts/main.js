3338fbcbfbf1365f16fd4ba6f72182920b3cfdaa
mattdlgado <lardeastudio@gmail.com>
2024-03-19 11:36:52 +0100

    Fix some errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,7 +43,7 @@ function horizontalScrollChallenges() {
       ease: "none",
       scrollTrigger: {
         trigger: ".pin-panel",
-        start: "top top",
+        start: "top 89px",
         end: "+=200%",
         pin: true,
         scrub: true,

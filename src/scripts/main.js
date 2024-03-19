c6ec2e78e5bc4d1f6e281f0a5f346e5d1646a624
mattdlgado <lardeastudio@gmail.com>
2024-03-19 12:57:10 +0100

    Add LenisJS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -86,3 +86,27 @@ function onResize() {
 
 window.addEventListener("resize", onResize);
 initFunctions();
+
+// Lenis
+import Lenis from "@studio-freight/lenis";
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)), // https://easings.net
+  direction: "vertical",
+  smooth: true,
+  smoothTouch: false,
+  touchMultiplier: 2,
+});
+
+//get scroll value
+lenis.on("scroll", ({ scroll, limit, velocity, direction, progress }) => {
+  console.log({ scroll, limit, velocity, direction, progress });
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
